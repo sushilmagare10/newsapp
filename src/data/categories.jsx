17d50a5300f64a172d36d77ca4e0daae589d59c3
Sushil Magare <sushilmagare10@gmail.com>
2024-06-22 00:58:28 +0530

    "added select by country"

--- src/data/categories.jsx
+++ src/data/categories.jsx
@@ -8,4 +8,20 @@ const categories = [
     "technology"
 ];
 
+
+export const countries = [
+  { code: 'us', name: 'United States' },
+  { code: 'gb', name: 'United Kingdom' },
+  { code: 'ca', name: 'Canada' },
+  // Add more countries as needed
+];
+
+export const languages = [
+  { code: 'en', name: 'English' },
+  { code: 'es', name: 'Spanish' },
+  { code: 'fr', name: 'French' },
+  // Add more languages as needed
+];
+
+
 export default categories;
