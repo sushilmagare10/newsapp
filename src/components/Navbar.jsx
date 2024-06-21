c3c072a254b47fdd62ff1764f8efafa8b0444f79
Sushil Magare <sushilmagare10@gmail.com>
2024-06-22 00:43:13 +0530

    "added select category"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,11 @@ import categories from '../data/categories'
 
 const Navbar = ({setCategory}) => {
   return (
-    <div className='flex w-full h-[90px] top-0 bg-white fixed px-8 justify-between items-center lg:visible  ' >
-    <div className=' px-4'>
-         Logo
-         </div>
+    <div className='flex w-full  bg-white px-8 justify-between items-center lg:visible  ' >
+  
     <ul className='flex justify-between text-lg capitalize  '>
     {categories.map((text,index)=>( 
-    <li key={index} className="mr-6 cursor-pointer font-semibold" onClick= {()=> setCategory(text)}>
+    <li key={index} className="mr-6 cursor-pointer px-4 py-2 bg-slate-200 rounded-full font-semibold" onClick= {()=> setCategory(text)}>
       <a key={index} 
       className="text-blue-500 hover:text-blue-800"  >{text}</a>
   </li>
--- src/components/NewsContent.jsx
+++ src/components/NewsContent.jsx
@@ -3,7 +3,7 @@ import NewsCard from './NewsCard'
 
 const NewsContent = ({newsArray, newsResults, loadMore, setLoadMore}) => {
   return (
-    <div className=' grid  grid-cols-4 gap-10 mt-24 row-auto'>
+    <div className=' grid  grid-cols-4 gap-10 mt-10 row-auto'>
       {newsArray.map((newsItem)=>(
             <NewsCard
                 newsItem={newsItem}
