f1d4d24294d1d22c08646fdbce369a3048042b99
Sushil Magare <sushilmagare10@gmail.com>
2024-06-22 20:15:06 +0530

    "proxy"

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  proxy: {
+    '/api': {
+      target: 'https://newsapi.org/v2',
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api/, ''),
+      headers: {
+        'X-Api-Key': 'YOUR_NEWS_API_KEY', 
+      },
+    },
+  },
 })
